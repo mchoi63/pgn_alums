f6f63a7bdcde45a231d948485cea87aad26e35df
mchoi63 <mingyuchoi@wustl.edu>
2019-09-02 17:42:16 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -35,7 +35,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'main.html',
+  signInSuccessUrl: 'database.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
